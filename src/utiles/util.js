4d6c3e9bbe1ce4063fcb3d30ebc3478a3e157884
MuzaffarBashir <mbas@ciklum.com>
2021-12-13 17:47:45 +0500

    success case for return error msg for mocked api post method

--- src/utiles/util.js
+++ src/utiles/util.js
@@ -1,1 +1,12 @@
+import axios from 'axios'
+export const fetchTodoUrl = 'http://localhost:8090'
+export const postTodoUrl = 'http://localhost:8090'
 
+
+export const insertTodo = async () => {
+  try {
+    return await axios.post(`${postTodoUrl}/handlerequest`)
+  } catch (e) {
+    return []
+  }
+}
